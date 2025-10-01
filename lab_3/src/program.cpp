1a6fd8a7f0bf395ec8154b269421a12e033a1a86
Stas Kuleshov <staskuleshov6@gmail.com>
2025-10-01 21:40:56 +0300

    fix

--- lab_3/src/program.cpp
+++ lab_3/src/program.cpp
@@ -34,22 +34,25 @@ Student *Program::createStudent() {
 void Program::addStudent() {
     if (count < 0) count = 0;
     if (students == nullptr) {
-        students = new Student *[capacity]{};
+        students = new Student *[capacity]();
     }
 
     if (count >= capacity) {
         int newCapacity = capacity * 2;
-        auto **newStudents = new Student *[newCapacity]{};
-        
+        auto **newStudents = new Student *[newCapacity]();
+
         for (int i = 0; i < count; i++) {
-            newStudents[i] = students[i];
+              if (students[i] != nullptr) { 
+                newStudents[i] = students[i];
+            }
         }
-        
+
         delete[] students;
+
         students = newStudents;
         capacity = newCapacity;
     }
-    
+
     Student *newStudent = createStudent();
     if (newStudent != nullptr) {
         students[count] = newStudent;
