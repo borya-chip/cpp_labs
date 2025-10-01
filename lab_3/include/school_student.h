01929feaedfa4546043d9563368dec18659c579c
Stas Kuleshov <staskuleshov6@gmail.com>
2025-10-01 18:47:45 +0300

    Fix some bugs

--- lab_3/include/school_student.h
+++ lab_3/include/school_student.h
@@ -4,14 +4,12 @@
 #include<string>
 #include "student.h"
 
-using namespace std;
-
 class SchoolStudent : public Student {
-    string school;
+    std::string school;
     int grade;
 
   public:
-    SchoolStudent(const string &name, const string &school, int grade);
+    SchoolStudent(const std::string &name, const std::string &school, int grade);
     SchoolStudent();
     void display() const override;
     void inputStudent() override;
