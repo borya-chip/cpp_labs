bed1a1c415e408eb15af19800dde1ea51e9288d3
Stas Kuleshov <staskuleshov6@gmail.com>
2025-10-15 20:22:15 +0300

    Test commit

--- lab_4/src/student.cpp
+++ lab_4/src/student.cpp
@@ -5,11 +5,11 @@ using namespace std;
 Student::Student(const string &name) : name(name) {
 }
 
+string Student::getName() const {
+   return name;
+}
+
 void Student::inputStudent() {
     cout << "Enter name: ";
     getline(cin, name);
-}
-
-string Student::getName() const {
-   return name;
 }
